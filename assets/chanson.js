f44322541c7a2e9fd7fc7095553bc6647052384f
Hitami860 <Hitami86000@gmail.com>
2024-09-12 14:43:54 +0200

    upd

--- assets/chanson.js
+++ assets/chanson.js
@@ -1,24 +1,34 @@
-let data =[]
+let data = []
 
 fetch('chanson.json')
   .then(response => response.json()
 
   )
-
   .then(data => {
 
     console.log(data);
 
+    let number = getRandomItem(data);
 
-    document.getElementById("anneeSortie").innerText = 'Annee de sortie : ' + data[0]["sortie"];
-    document.getElementById("lyrics").innerText = data[0]["parole"];
-    let str = data[0]["parole"];
-    data[0]["parole"] = str.split(' ');
+    document.getElementById("anneeSortie").innerText = 'Annee de sortie : ' + data[number]["sortie"];
+    // document.getElementById("lyrics").innerText = data[number]["parole"];
 
-    
-    let mot = data[0]["parole"] = str.split(' ');
+    let mot = data[number]["parole"].split(' ');
+
     console.log(mot);
 
+    mot.forEach(datas => {
+
+      let motSpan = document.createElement("span");
+      document.getElementById("lyrics");
+      motSpan.setAttribute("class","unselectable",)
+      motSpan.style.backgroundColor = "black";
+      motSpan.style.marginRight = "6px";
+      motSpan.innerText = datas;
+      document.body.appendChild(motSpan);
+
+      console.log(motSpan);
+    });
 
   })
   .catch(error => {
@@ -26,16 +36,42 @@ fetch('chanson.json')
   });
 
 
-function getRandomItem() {
-
-    return randomChanson = Math.floor(Math.random() * data.length);
-
-}
-
-console.log(getRandomItem);
 
 
-function startGame() {
+function getRandomItem(data) {
+
+  return Math.floor(Math.random() * data.length);
 
 }
 
+
+
+
+function checkWord() {
+
+  let motUser = document.getElementById('userMot').value.trim().toLowerCase();
+  let checkBtn = document.getElementById('btntrouve').addEventListener('click', () => {
+    if (!motUser) {
+        alert('Veuillez ecrire dun mot dans le champs requis !');
+        return;
+    }
+
+}
+
+)};
+
+
+
+
+function checkTitre() {
+
+ let userTitre =  document.getElementById('titreChanson').value.trim().toLowerCase();
+ let btntrouve = document.getElementById('btntrouve').addEventListener('click', () => {
+      if (!userTitre) {
+          alert('Veuillez ecrire un titre de chanson !');
+          return;
+      }
+
+}
+
+)};
